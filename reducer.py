48fde09705657b6d53e663d4927f83b8722473f1
unknown <otaviojacobi@gmail.com>
2020-12-11 13:36:41 -0300

    lowering dataset size

---
+++ reducer.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import os, sys
+from tqdm import tqdm
+
+path = "./DUMMY/PLEASE_FILL_THIS/"
+dirs = os.listdir( path )
+
+print(dirs)
+
+for item in tqdm(dirs):
+    if os.path.isfile(path+item):
+        im = Image.open(path+item).convert("RGB")
+        f, e = os.path.splitext(path+item)
+        imResize = im.resize((512, 512), Image.ANTIALIAS)
+        os.remove(path+item)
+        imResize.save(f + '.jpg', 'JPEG', quality=90)
